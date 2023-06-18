2838f08e1f7dc678161304efe77e42b00d995bef
tjmcdono97 <95113527+tjmcdono97@users.noreply.github.com>
2023-06-18 12:38:18 -0700

    main.tf updated

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,6 +14,13 @@ resource "aws_s3_bucket" "site" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 # Configure public access settings for the site bucket
