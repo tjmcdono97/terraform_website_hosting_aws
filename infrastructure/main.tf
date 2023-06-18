86a595705560f5c992fde142a971a47cfb944357
tjmcdono97 <95113527+tjmcdono97@users.noreply.github.com>
2023-06-17 20:06:48 -0700

    updated to new formatting style along with workflows added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,9 +14,9 @@ resource "aws_s3_bucket" "site" {
 resource "aws_s3_bucket_public_access_block" "access_block" {
   bucket = aws_s3_bucket.site.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -60,9 +60,9 @@ resource "aws_s3_bucket" "www" {
 resource "aws_s3_bucket_public_access_block" "www_access_block" {
   bucket = aws_s3_bucket.www.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
