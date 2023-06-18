bc1fb8a0e02ca4ecdee092d68b5cb998b5995975
tjmcdono97 <95113527+tjmcdono97@users.noreply.github.com>
2023-06-18 12:30:24 -0700

    modified main.tf terraform fmt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,14 +41,14 @@ resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
 
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = [
+        Resource = [
           aws_s3_bucket.site.arn,
           "${aws_s3_bucket.site.arn}/*",
         ]
