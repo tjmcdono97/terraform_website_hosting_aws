5860ca09c976a3a8ab4f13d7f97538bbf67bcc21
calders1 <43142574+calders1@users.noreply.github.com>
2023-11-17 12:01:07 -0600

    fmt outputs.tf

--- infrastructure/backend-setup/outputs.tf
+++ infrastructure/backend-setup/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.terraform_state.id
+  value       = aws_s3_bucket.terraform_state.id
   description = "The name of the S3 bucket to be used for Terraform state storage"
 }
 
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform_locks.name
+  value       = aws_dynamodb_table.terraform_locks.name
   description = "The name of the DynamoDB table to be used for Terraform state locking"
 }
